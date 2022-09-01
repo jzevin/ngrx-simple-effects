919968416630a6d68abb86a164e9f701ec063268
Jeremy Zevin <jzevin@nbme.org>
2022-09-01 17:50:34 -0400

    add api service

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { slidesReducer } from './state/slides.state.reducers';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {}),
+    HttpClientModule,
+    StoreModule.forRoot({slides: slidesReducer}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
